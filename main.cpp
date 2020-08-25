2a7fb59c442530fcc48620df08df4e6c09a6e8b9
knowledge35 <ibomyakmc@hotmail.com>
2020-08-25 07:18:00 +0300

    ol artik

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int main()
+{
+	int a;
+	a=1;
+	while(a<10){
+		cout<<"merhaba"<<a<<endl;
+		a++;
+		
+	}
+	cout<<a<<endl;
+   getch();
+   return 0;
+}
